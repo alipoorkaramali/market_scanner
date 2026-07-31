d47642dbf30f9c08598391be8e7710612c046db7
ali <ali13741393.ap@gmail.com>
2026-07-31 14:49:24 +0330

    تست config.py

--- Config/config.py
+++ Config/config.py
@@ -1,7 +1,8 @@
 SYMBOLS = [
     'BTC/USDT', 'ETH/USDT', 'BNB/USDT',
-    'SOL/USDT', 'XRP/USDT', 'ADA/USDT'
+    'SOL/USDT', 'XRP/USDT', 'ADA/USDT',
+    'DOGE/USDT', 'DOT/USDT', 'LINK/USDT'
 ]
 
-TIMEFRAME = '1h'  # بازه زمانی برای تحلیل
-LOOKBACK_DAYS = 30  # تعداد روز داده برای هر جفت‌ارز
+TIMEFRAME = '4h'        # 4h, 1d, 1w
+LOOKBACK_DAYS = 180     # تعداد کندل (برای 4h، 180 کندل = 30 روز)
