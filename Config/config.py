f71eca809ffe12fec9eb9045f90ba56a6a34d6db
ali <ali13741393.ap@gmail.com>
2026-07-31 14:42:25 +0330

    ساخت config.py

--- Config/config.py
+++ Config/config.py
@@ -1,1 +1,7 @@
+SYMBOLS = [
+    'BTC/USDT', 'ETH/USDT', 'BNB/USDT',
+    'SOL/USDT', 'XRP/USDT', 'ADA/USDT'
+]
 
+TIMEFRAME = '1h'  # بازه زمانی برای تحلیل
+LOOKBACK_DAYS = 30  # تعداد روز داده برای هر جفت‌ارز
